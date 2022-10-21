12314d20c2ef96d50405895390f393f43f3e3cee
odinokun <odinokun@gmail.com>
2022-10-21 22:43:08 +0200

    did regist section

--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -39,209 +39,251 @@ module.exports = function () {
     }
   });
 
+  const registrSlider1 = new Swiper('#registr-sec__slider-01', {
+    spaceBetween: 20,
+    grabCursor: false,
+    slidesPerView: 'auto',
+    freeMode: true,
+    freeModeMomentum: true,
+    loop: true,
+    speed: 6000,
+    mousewheelControl: false,
+    keyboardControl: false,
+    autoplay: {
+      delay: 1,
+      disableOnInteraction: true
+    },
+    breakpoints: {
+      769: {
+        direction: "vertical",
+      }
+    }
+  });
+
+  const registrSlider2 = new Swiper('#registr-sec__slider-02', {
+    spaceBetween: 20,
+    grabCursor: false,
+    slidesPerView: 'auto',
+    freeMode: true,
+    freeModeMomentum: true,
+    loop: true,
+    speed: 6000,
+    mousewheelControl: false,
+    keyboardControl: false,
+    autoplay: {
+      delay: 1,
+      disableOnInteraction: true,
+      reverseDirection: true
+    },
+    breakpoints: {
+      769: {
+        direction: "vertical",
+      }
+    }
+  });
 
 
   //******************************
-    //***** TEMP SLIDERS *************
-    //******************************
+  //***** TEMP SLIDERS *************
+  //******************************
 
-    const blogSlider = new Swiper('.blog-slider', {
-      scrollbar: {
-        el: '.swiper-scrollbar',
-        draggable: true,
+  const blogSlider = new Swiper('.blog-slider', {
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      draggable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    spaceBetween: 20,
+    slidesPerView: 1.1,
+    breakpoints: {
+      769: {
+        spaceBetween: 20,
+        slidesPerView: 2.3,
       },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+      1025: {
+        spaceBetween: 30,
+        slidesPerView: 3,
       },
-      spaceBetween: 20,
-      slidesPerView: 1.1,
-      breakpoints: {
-        769: {
-          spaceBetween: 20,
-          slidesPerView: 2.3,
-        },
-        1025: {
-          spaceBetween: 30,
-          slidesPerView: 3,
-        },
-        1400: {
-          spaceBetween: 30,
-          slidesPerView: 4,
-        }
+      1400: {
+        spaceBetween: 30,
+        slidesPerView: 4,
       }
-    });
+    }
+  });
 
-    const docSlider = new Swiper('.doc-slider', {
-      scrollbar: {
-        el: '.swiper-scrollbar',
-        draggable: true,
+  const docSlider = new Swiper('.doc-slider', {
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      draggable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    spaceBetween: 20,
+    slidesPerView: 1.1,
+    breakpoints: {
+      769: {
+        spaceBetween: 20,
+        slidesPerView: 2.3,
       },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+      1025: {
+        spaceBetween: 30,
+        slidesPerView: 3,
       },
-      spaceBetween: 20,
-      slidesPerView: 1.1,
-      breakpoints: {
-        769: {
-          spaceBetween: 20,
-          slidesPerView: 2.3,
-        },
-        1025: {
-          spaceBetween: 30,
-          slidesPerView: 3,
-        },
-        1400: {
-          spaceBetween: 30,
-          slidesPerView: 4,
-        }
+      1400: {
+        spaceBetween: 30,
+        slidesPerView: 4,
       }
-    });
+    }
+  });
 
-    const shopTop = new Swiper('.shop-top-slider', {
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+  const shopTop = new Swiper('.shop-top-slider', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    spaceBetween: 10,
+    slidesPerView: 1.02,
+    breakpoints: {
+      769: {
+        spaceBetween: 10,
+        slidesPerView: 2.3,
       },
-      spaceBetween: 10,
-      slidesPerView: 1.02,
-      breakpoints: {
-        769: {
-          spaceBetween: 10,
-          slidesPerView: 2.3,
-        },
-        1025: {
-          spaceBetween: 15,
-          slidesPerView: 3,
-        },
-        1400: {
-          spaceBetween: 15,
-          slidesPerView: 4,
-        }
+      1025: {
+        spaceBetween: 15,
+        slidesPerView: 3,
+      },
+      1400: {
+        spaceBetween: 15,
+        slidesPerView: 4,
       }
-    });
+    }
+  });
 
-    const capabilitiesSlider = new Swiper('.capabilities-slider', {
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true
+  const capabilitiesSlider = new Swiper('.capabilities-slider', {
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    spaceBetween: 20,
+    slidesPerView: 1,
+    breakpoints: {
+      600: {
+        spaceBetween: 20,
+        slidesPerView: 2,
       },
-      spaceBetween: 20,
-      slidesPerView: 1,
-      breakpoints: {
-        600: {
-          spaceBetween: 20,
-          slidesPerView: 2,
-        },
-        769: {
-          spaceBetween: 20,
-          slidesPerView: 3,
-        },
-        1025: {
-          spaceBetween: 20,
-          slidesPerView: 4,
-        }
+      769: {
+        spaceBetween: 20,
+        slidesPerView: 3,
+      },
+      1025: {
+        spaceBetween: 20,
+        slidesPerView: 4,
       }
-    });
+    }
+  });
 
-    const shopProductThumb = new Swiper(".shop-product__slider--thumb", {
-      spaceBetween: 8,
-      slidesPerView: 4,
-      freeMode: true,
-      direction: "vertical",
-      watchSlidesProgress: true,
-    });
-    const shopProductMain = new Swiper(".shop-product__slider--main", {
-      thumbs: {
-        swiper: shopProductThumb,
-      },
-    });
+  const shopProductThumb = new Swiper(".shop-product__slider--thumb", {
+    spaceBetween: 8,
+    slidesPerView: 4,
+    freeMode: true,
+    direction: "vertical",
+    watchSlidesProgress: true,
+  });
+  const shopProductMain = new Swiper(".shop-product__slider--main", {
+    thumbs: {
+      swiper: shopProductThumb,
+    },
+  });
 
-    const reviewsSlider = new Swiper('.reviews-slider', {
-      spaceBetween: 20,
-      grabCursor: false,
-      slidesPerView: 'auto',
-      freeMode: true,
-      freeModeMomentum: false,
-      loop: true,
-      speed: 4000,
-      mousewheelControl: false,
-      keyboardControl: false,
-      autoplay: {
-        delay: 1,
-        disableOnInteraction: true
-      },
-    });
+  const reviewsSlider = new Swiper('.reviews-slider', {
+    spaceBetween: 20,
+    grabCursor: false,
+    slidesPerView: 'auto',
+    freeMode: true,
+    freeModeMomentum: false,
+    loop: true,
+    speed: 4000,
+    mousewheelControl: false,
+    keyboardControl: false,
+    autoplay: {
+      delay: 1,
+      disableOnInteraction: true
+    },
+  });
 
-    const trustedSlider = new Swiper('.trusted-slider', {
-      spaceBetween: 20,
-      grabCursor: true,
-      slidesPerView: 'auto',
-      freeMode: true,
-      freeModeMomentum: false,
-      loop: true,
-      speed: 4000,
-      mousewheelControl: true,
-      keyboardControl: true,
-      autoplay: {
-        delay: 1,
-        disableOnInteraction: true
-      },
-    });
+  const trustedSlider = new Swiper('.trusted-slider', {
+    spaceBetween: 20,
+    grabCursor: true,
+    slidesPerView: 'auto',
+    freeMode: true,
+    freeModeMomentum: false,
+    loop: true,
+    speed: 4000,
+    mousewheelControl: true,
+    keyboardControl: true,
+    autoplay: {
+      delay: 1,
+      disableOnInteraction: true
+    },
+  });
 
-    const howItWorksSlider = new Swiper('.how-it-works-slider', {
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      spaceBetween: 20,
-      slidesPerView: 1.1,
-      mousewheel: {
-        invert: false,
-        releaseOnEdges: true,
-        // sensitivity:10,
-        // thresholdDelta:10,
-        // thresholdTime:10,
-      },
-      breakpoints: {
-        769: {
-          direction: 'vertical',
-          effect: 'fade',
-          fadeEffect: {
-            crossFade: true
-          },
-          spaceBetween: 0,
-          slidesPerView: 1,
-          // mousewheel: true,
-        }
+  const howItWorksSlider = new Swiper('.how-it-works-slider', {
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    spaceBetween: 20,
+    slidesPerView: 1.1,
+    mousewheel: {
+      invert: false,
+      releaseOnEdges: true,
+      // sensitivity:10,
+      // thresholdDelta:10,
+      // thresholdTime:10,
+    },
+    breakpoints: {
+      769: {
+        direction: 'vertical',
+        effect: 'fade',
+        fadeEffect: {
+          crossFade: true
+        },
+        spaceBetween: 0,
+        slidesPerView: 1,
+        // mousewheel: true,
       }
-    });
+    }
+  });
 
-    const guaranteesThumb = new Swiper(".our-guarantees__tabs", {
-      // spaceBetween: 8,
-      slidesPerView: 4,
-      direction: "vertical",
-      speed: 2000,
-      autoplay: {
-        delay: 4000,
-        disableOnInteraction: true
-      },
-    });
-    const guaranteesSlider = new Swiper('.our-guarantees-slider', {
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      slidesPerView: 1,
-      effect: 'fade',
-      speed: 500,
-      autoplay: {
-        delay: 4000,
-        disableOnInteraction: true
-      },
-      thumbs: {
-        swiper: guaranteesThumb,
-      },
-    });
+  const guaranteesThumb = new Swiper(".our-guarantees__tabs", {
+    // spaceBetween: 8,
+    slidesPerView: 4,
+    direction: "vertical",
+    speed: 2000,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: true
+    },
+  });
+  const guaranteesSlider = new Swiper('.our-guarantees-slider', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 1,
+    effect: 'fade',
+    speed: 500,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: true
+    },
+    thumbs: {
+      swiper: guaranteesThumb,
+    },
+  });
 };
